50dd39de07871c8169b16e6299d1d58cffc38105
Keith Brister <k-brister@northwestern.edu>
2013-02-08 23:05:52 +0000

    est_move_time_wait needs a null terminated list of motors
    git-svn-id: file:///contrabass/repos/svn/pmac/trunk@3032 66ca1eeb-be3a-49d5-93d8-8bdade05efff

--- lstest.c
+++ lstest.c
@@ -23,7 +23,7 @@ void lstest_lspmac_est_move_time() {
   err = lspmac_est_move_time( &move_time, &mmask, omega, 0, NULL, 360., NULL);
   lslogging_log_message( "lstest_lspmac_est_move_time: omega 360  move_time=%f  err=%d", move_time, err);
   
-  if( lspmac_est_move_time_wait( move_time + fudge, mmask)) {
+  if( lspmac_est_move_time_wait( move_time + fudge, mmask, NULL)) {
     lslogging_log_message( "lstest_lspmac_est_move_time: timed out");
     return;
   }
@@ -31,7 +31,7 @@ void lstest_lspmac_est_move_time() {
   err = lspmac_est_move_time( &move_time, &mmask, aperz, 0, "Cover", 0., NULL);
   lslogging_log_message( "lstest_lspmac_est_move_time: aperz Cover move_time=%f err=%d", move_time, err);
 
-  if( lspmac_est_move_time_wait( move_time + fudge, mmask)) {
+  if( lspmac_est_move_time_wait( move_time + fudge, mmask, NULL)) {
     lslogging_log_message( "lstest_lspmac_est_move_time: timed out");
     return;
   }
@@ -39,7 +39,7 @@ void lstest_lspmac_est_move_time() {
   err = lspmac_est_move_time( &move_time, &mmask, aperz, 0, "In", 0., NULL);
   lslogging_log_message( "lstest_lspmac_est_move_time: aperz In    move_time=%f err=%d", move_time, err);
 
-  if( lspmac_est_move_time_wait( move_time + fudge, mmask)) {
+  if( lspmac_est_move_time_wait( move_time + fudge, mmask, NULL)) {
     lslogging_log_message( "lstest_lspmac_est_move_time: timed out");
     return;
   }
@@ -47,7 +47,7 @@ void lstest_lspmac_est_move_time() {
   err = lspmac_est_move_time( &move_time, &mmask, capz, 0, "Cover", 0., NULL);
   lslogging_log_message( "lstest_lspmac_est_move_time: capz Cover  move_time=%f err=%d", move_time, err);
 
-  if( lspmac_est_move_time_wait( move_time + fudge, mmask)) {
+  if( lspmac_est_move_time_wait( move_time + fudge, mmask, NULL)) {
     lslogging_log_message( "lstest_lspmac_est_move_time: timed out");
     return;
   }
@@ -56,7 +56,7 @@ void lstest_lspmac_est_move_time() {
   err = lspmac_est_move_time( &move_time, &mmask, capz, 0, "In", 0., NULL);
   lslogging_log_message( "lstest_lspmac_est_move_time: capz In     move_time=%f err=%d", move_time, err);
 
-  if( lspmac_est_move_time_wait( move_time + fudge, mmask)) {
+  if( lspmac_est_move_time_wait( move_time + fudge, mmask, NULL)) {
     lslogging_log_message( "lstest_lspmac_est_move_time: timed out");
     return;
   }
@@ -64,14 +64,14 @@ void lstest_lspmac_est_move_time() {
   err = lspmac_est_move_time( &move_time, &mmask, apery, 0, "In", 0.0, aperz, 0, "In", 0.0, capy, 0, "In", 0.0, capz, 0, "In", 0.0, scint, 0, "Scintillator", 0.0, NULL);
   lslogging_log_message( "lstest_lspmac_est_move_time: apery In aperz In capy In capz In scint Scintillator move_time=%f err=%d", move_time, err);
 
-  if( lspmac_est_move_time_wait( move_time + fudge, mmask)) {
+  if( lspmac_est_move_time_wait( move_time + fudge, mmask, NULL)) {
     lslogging_log_message( "lstest_lspmac_est_move_time: timed out");
     return;
   }
   err = lspmac_est_move_time( &move_time, &mmask, apery, 0, "In", 0.0, aperz, 0, "Cover", 0.0, capy, 0, "In", 0.0, capz, 0, "Cover", 0.0, scint, 0, "Cover", 0.0, NULL);
   lslogging_log_message( "lstest_lspmac_est_move_time: apery Cover aperz Cover capy Cover capz Cover scint Cover move_time=%f err=%d", move_time, err);
 
-  if( lspmac_est_move_time_wait( move_time + fudge, mmask)) {
+  if( lspmac_est_move_time_wait( move_time + fudge, mmask, NULL)) {
     lslogging_log_message( "lstest_lspmac_est_move_time: timed out");
     return;
   }
@@ -80,7 +80,7 @@ void lstest_lspmac_est_move_time() {
 			      omega, 0, "manualMount", 0.0, kappa, 0, "manualMount", 0.0, NULL);
   lslogging_log_message( "lstest_lspmac_est_move_time: apery In aperz In capy In capz In scint Scintillator omega manualMount kappa Manualmount move_time=%f err=%d", move_time, err);
 
-  if( lspmac_est_move_time_wait( move_time + fudge, mmask)) {
+  if( lspmac_est_move_time_wait( move_time + fudge, mmask, NULL)) {
     lslogging_log_message( "lstest_lspmac_est_move_time: timed out");
     return;
   }
