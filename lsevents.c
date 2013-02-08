2fd276d14e5f19734eae3977747b7b411e78908c
Keith Brister <k-brister@northwestern.edu>
2013-02-08 23:09:48 +0000

    save point (not working).  Making send_events O(1)
    git-svn-id: file:///contrabass/repos/svn/pmac/trunk@3035 66ca1eeb-be3a-49d5-93d8-8bdade05efff

--- lsevents.c
+++ lsevents.c
@@ -22,6 +22,14 @@ static lsevents_queue_t lsevents_queue[LSEVENTS_QUEUE_LENGTH];		//!< simple list
 static unsigned int lsevents_queue_on  = 0;				//!< next queue location to write
 static unsigned int lsevents_queue_off = 0;				//!< next queue location to read
 
+//
+// Store a list of event n_events names in a hash table
+// of maximum length max_events
+//
+static int lsevents_max_events = 1024;
+static int lsevents_n_events   =    0;
+static struct hsearch_data lsevents_event_name_ht;
+
 /** Linked list of event listeners.
  */
 typedef struct lsevents_listener_struct {
@@ -32,6 +40,26 @@ typedef struct lsevents_listener_struct {
 } lsevents_listener_t;
 
 static lsevents_listener_t *lsevents_listeners_p = NULL;	//!< Pointer to the first item in the link list of listeners
+
+/** linked list of callbacks for each event
+ *   Used to find all the callbacks for a given event
+ */
+typedef struct lsevents_callbacks_struct {
+  struct lsevents_callbacks_struct *next;
+  void (*cb)( char *);
+} lsevents_callbacks_t;
+
+/** linked list of all the event names
+ *  used to regenerate the hash table
+ */
+typedef struct lsevents_event_names_struct {
+  struct lsevents_event_names_struct *next;
+  char *event;					// event string
+  lsevents_callbacks_t *cbl;			// callback list
+} lsevents_event_names_t;
+static lsevents_event_names_t *lsevents_event_names = NULL;
+
+
 
 static pthread_t       lsevents_thread;			//!< thread to run the event queue
 static pthread_mutex_t lsevents_listener_mutex;		//!< mutex to protect the listener linked list
@@ -61,7 +89,6 @@ void lsevents_send_event( char *fmt, ...) {
 
   pthread_cond_signal(  &lsevents_queue_cond);
   pthread_mutex_unlock( &lsevents_queue_mutex);
-
 }
 
 
@@ -106,6 +133,9 @@ void lsevents_add_listener( char *event, void (*cb)(char *)) {
   lsevents_listeners_p = new;
   pthread_mutex_unlock( &lsevents_listener_mutex);
 
+  // TODO: go through list of event names and perhaps add ourselves to the callback list
+  //
+
   lslogging_log_message( "lsevents_add_listener: added listener for event %s", event);
 
 }
@@ -144,6 +174,11 @@ void lsevents_remove_listener (char *event, void (*cb)(char *)) {
       free( current->raw_regexp);
     free(current);
   }
+
+  // TODO: go through list of event names and perhaps remove ourselves to the callback list
+  //
+
+
 }
 
 /** Our worker
@@ -153,9 +188,12 @@ void *lsevents_worker(
 		     void *dummy
 		     ) {
   
-  //  char *event;
-  lsevents_queue_t *ep;
   lsevents_listener_t *p;
+  ENTRY entry_in, *entry_outp;
+  char *event;
+  int err;
+  lsevents_callbacks_t *last_cb, *new_cb, *cbi;
+  lsevents_event_names_t *new_event_name, *event_name_list;
 
   while( 1) {
     pthread_mutex_lock( &lsevents_queue_mutex);
@@ -167,10 +205,9 @@ void *lsevents_worker(
       pthread_cond_wait( &lsevents_queue_cond, &lsevents_queue_mutex);
 
     //
-    // copy event string since the value in the queue may change when
-    // we unlock the mutex
+    // Get our event name
     //
-    ep = &(lsevents_queue[(lsevents_queue_off++) % LSEVENTS_QUEUE_LENGTH]);
+    event = lsevents_queue[(lsevents_queue_off++) % LSEVENTS_QUEUE_LENGTH].evp;
 
     //
     // let the send event process know there is room on the queue again
@@ -178,30 +215,77 @@ void *lsevents_worker(
     pthread_cond_signal(  &lsevents_queue_cond);
     pthread_mutex_unlock( &lsevents_queue_mutex);
 
+
+    // Have we seen this event before?
     //
-    // Find the callbacks and, well, call them back
+    entry_in.key  = event;
+    entry_in.data = NULL;
+
     //
-    // TODO:
-    //
-    // Yes, this is O(N).
-    //
-    // Plan to make this O(1):
-    //  track actual event names from send_event
-    //  match listeners for new event names
-    //  store matchs in hash table
-    //
-    // That makes send_event for new events O(N)
-    // but O(1) otherwise, O(N) for add_listener, and O(1) here.
+    // protect the hash table: add_listener and remove_listener may also mess with it
     //
     pthread_mutex_lock( &lsevents_listener_mutex);
-    for( p = lsevents_listeners_p; p != NULL; p = p->next) {
-      if( regexec( &p->re, ep->evp, 0, NULL, 0) == 0) {
-	p->cb( ep->evp);
+    err = hsearch_r( entry_in, FIND, &entry_outp, &lsevents_event_name_ht);
+    if( err == 0) {
+      if( errno != ESRCH) {
+	lslogging_log_message( "lsevents_worker: hsearch_r returned %d: %s", errno, strerror( errno));
+	free( event);
+	continue;
+      } else {
+	//
+	// Not Found
+	//
+	// Make an array of matching events;
+	//
+	last_cb = NULL;
+	
+	for( p=lsevents_listeners_p; p != NULL; p = p->next) {
+	  if( regexec( &p->re, event, 0, NULL, 0) == 0) {
+	    new_cb = calloc( 1, sizeof( lsevents_callbacks_t));
+	    new_cb->cb = p->cb;
+	    new_cb->next = last_cb;
+	    last_cb = new_cb;
+	  }
+	}
+	new_event_name = calloc( 1, sizeof( lsevents_event_names_t));
+	new_event_name->event = strdup( event);
+	new_event_name->cbl   = last_cb;
+	new_event_name->next  = lsevents_event_names;
+	lsevents_event_names  = new_event_name;
+
+	entry_in.key  = new_event_name->event;
+	entry_in.data = new_event_name;
+	hsearch_r( entry_in, ENTER, &entry_outp, &lsevents_event_name_ht);
+	if( ++lsevents_n_events  >= lsevents_max_events) {
+	  hdestroy_r( &lsevents_event_name_ht);
+	  lsevents_max_events *= 2;
+	  hcreate_r( lsevents_max_events * 2, &lsevents_event_name_ht);
+	  for( new_event_name = lsevents_event_names; new_event_name != NULL; new_event_name = new_event_name->next) {
+	    entry_in.key  = new_event_name->event;
+	    entry_in.data = new_event_name;
+	    hsearch_r( entry_in, ENTER, &entry_outp, &lsevents_event_name_ht);
+	  }
+	}
+	err = hsearch_r( entry_in, FIND, &entry_outp, &lsevents_event_name_ht);
+	if( err == 0) {
+	  lslogging_log_message( "lsevents_worker: For some reason we can find a hash table entry for event '%s'", event);
+	  free( event);
+	  pthread_mutex_unlock( &lsevents_listener_mutex);
+	  continue;
+	}
       }
     }
-    free( ep->evp);
-    
     pthread_mutex_unlock( &lsevents_listener_mutex);
+
+
+    // call our callbacks
+    //
+    event_name_list = entry_in.data;
+    for( cbi = event_name_list->cbl; cbi != NULL; cbi = cbi->next) {
+      cbi->cb( event);
+    }
+
+    free( event);
   }
   return NULL;
 }
@@ -212,6 +296,17 @@ void lsevents_init() {
   pthread_mutex_init( &lsevents_queue_mutex, NULL);
   pthread_cond_init(  &lsevents_queue_cond, NULL);
   pthread_mutex_init( &lsevents_listener_mutex, NULL);
+
+  hcreate_r( 2*lsevents_max_events, &lsevents_event_name_ht);
+
+
+       int hcreate_r(size_t nel, struct hsearch_data *htab);
+
+       int hsearch_r(ENTRY item, ACTION action, ENTRY **retval,
+                     struct hsearch_data *htab);
+
+       void hdestroy_r(struct hsearch_data *htab);
+  
 }
 
 /** Start up the thread and get out of the way.
