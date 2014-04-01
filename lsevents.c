6369256437d1c8fd7f39f5acb69233219be5a493
Keith Brister <k-brister@northwestern.edu>
2014-04-01 20:20:11 +0000

    commented out some logging to make the program less chatty
    git-svn-id: file:///contrabass/repos/svn/pmac/trunk@3350 66ca1eeb-be3a-49d5-93d8-8bdade05efff

--- lsevents.c
+++ lsevents.c
@@ -121,7 +121,6 @@ void lsevents_add_listener( char *raw_regexp, void (*cb)(char *)) {
       exit( -1);
     }
     regerror( err, &new->re, errbuf, nerrbuf);
-    //    lslogging_log_message( "lsevents_add_listener: %s", errbuf);
     free( errbuf);
     free( new);
     return;
@@ -145,8 +144,6 @@ void lsevents_add_listener( char *raw_regexp, void (*cb)(char *)) {
   
 
   pthread_mutex_unlock( &lsevents_listener_mutex);
-
-  //  lslogging_log_message( "lsevents_add_listener: added listener for event '%s'", raw_regexp);
 
 }
 
@@ -252,7 +249,6 @@ lsevents_callbacks_t *lsevents_register_event( char *event) {
     return NULL;
   }
 
-  //  lslogging_log_message( "lsevents_register_event: adding event '%s'", event);
   //
   // Not Found
   //
@@ -296,7 +292,6 @@ lsevents_callbacks_t *lsevents_register_event( char *event) {
 
   if( ++lsevents_n_events  >= lsevents_max_events) {
     hdestroy_r( &lsevents_event_name_ht);
-    lslogging_log_message( "lsevents_register_event: Increasing event name hash table to %d. lsevents_n_events=%d", 2 * lsevents_max_events, lsevents_n_events);
     lsevents_max_events *= 2;
     hcreate_r( lsevents_max_events * 2, &lsevents_event_name_ht);
     for( enp = lsevents_event_names; enp != NULL; enp = enp->next) {
@@ -305,7 +300,6 @@ lsevents_callbacks_t *lsevents_register_event( char *event) {
       hsearch_r( entry_in, ENTER, &entry_outp, &lsevents_event_name_ht);
     }
   }
-  //  lslogging_log_message( "lsevents_register_event: added event '%s'", event);
   pthread_mutex_unlock( &lsevents_listener_mutex);
   return new_event_name->cbl;
 }  
